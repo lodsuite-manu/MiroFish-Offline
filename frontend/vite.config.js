d3936b8375ae47322bd64bbbca5eb0644b20e006
lodsuite-manu <manuel@hashtagmarketing.ch>
2026-04-03 18:52:06 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
     port: 3000,
-    open: true,
+    open: false,
+    allowedHosts: ['.sslip.io'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
