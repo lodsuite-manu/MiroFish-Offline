0560260fb2164c4ee4d256ace7407bef9b7cc367
lodsuite-manu <manuel@hashtagmarketing.ch>
2026-04-03 19:23:29 +0200

    Update Flask app run configuration

--- backend/run.py
+++ backend/run.py
@@ -42,7 +42,7 @@ def main():
     debug = Config.DEBUG
 
     # Start service
-    app.run(host=host, port=port, debug=debug, threaded=True)
+    app.run(host="0.0.0.0", port=5001, debug=False, use_reloader=False)
 
 
 if __name__ == '__main__':
